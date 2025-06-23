d245f213a255ec815e695cbc9877b2703bf1ff57
vanhhhhhhhhhhhh <163091992+vanhhhhhhhhhhhh@users.noreply.github.com>
2025-06-23 09:20:18 +0700

    change

--- backend/router/apiImport.js
+++ backend/router/apiImport.js
@@ -4,6 +4,4 @@ const middleware = require('../controller/middleware')
 
 router.get('/warehouse', middleware.verifyToken, importController.listWarehouse)
 
-router.get('/product', middleware.verifyToken, importController.listProduct)
-
 module.exports = router
