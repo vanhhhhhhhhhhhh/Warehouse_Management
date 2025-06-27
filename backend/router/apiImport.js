711cfcdbd5107090a2ca8eccbde100e5e098a3a9
vanhhhhhhhhhhhh <163091992+vanhhhhhhhhhhhh@users.noreply.github.com>
2025-06-27 17:06:17 +0700

    Backend importController

--- backend/router/apiImport.js
+++ backend/router/apiImport.js
@@ -4,4 +4,10 @@ const middleware = require('../controller/middleware')
 
 router.get('/warehouse', middleware.verifyToken, importController.listWarehouse)
 
+router.get('/product', middleware.verifyToken, importController.listProduct)
+
+router.post('/intoWarehouse', middleware.verifyToken, importController.importIntoWarehouse)
+
+router.get('/history', middleware.verifyToken, importController.historyImport)
+
 module.exports = router
